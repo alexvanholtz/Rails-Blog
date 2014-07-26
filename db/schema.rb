c41e0da33a53ff6f586853603128c1e9ece49fea
Alex Van Holtz <alexvanholtz@gmail.com>
2014-07-26 15:05:54 -0400

    Adding location and excerpt to Article

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140726185506) do
+ActiveRecord::Schema.define(version: 20140726190305) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20140726185506) do
     t.datetime "published_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "excerpt"
+    t.string   "location"
   end
 
 end
